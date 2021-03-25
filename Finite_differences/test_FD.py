6d13f6e779cbcef5f2977f54e0dd7995111239ba
Lennaert van Veen <lennaert.vanveen@uoit.ca>
2021-03-25 15:11:22 -0400

    Updated test_FD to include our homework solution and the typical V-shaped error curve of finite differencing.

--- Finite_differences/test_FD.py
+++ Finite_differences/test_FD.py
@@ -15,7 +15,7 @@ n = 4    # grid points are labelled 0 .. n
 errs = np.empty((0,2))
 
 # Loop over increasingly fine grids:
-for k in range(12):
+for k in range(21):
     xs = np.linspace(l,r,n+1) # Set up the grid: xs[i] = l + i * h  (i=0..n)
     h = (r-l)/float(n)        # Grid spacing
     ys = f(xs)                # Find function and its derivative on the grid
@@ -23,7 +23,14 @@ for k in range(12):
     fddys = np.zeros(n+1)     # Compute the finite difference approximation
     for i in range(1,n):      # on the interior grid points
         fddys[i] = (ys[i+1]-ys[i-1])/(2.0*h)
-    errs = np.append(errs,[[h,np.linalg.norm(dys[1:n] - fddys[1:n],np.inf)]],0) # Remember the error - use the inf norm so the number of grid points does not matter
+    # Illustration of cancellation error: two functions values almost coincide and we take the difference:
+    if n==2**20:
+        print('h=%e ys[1000]=%14.10f ys[1002]=%14.10f' % (h,ys[1000],ys[1002]))
+    # One-sided approximation at the boundaries (we derived these to have O(h^2) error!)
+    fddys[0] = (-3.0 * ys[0] + 4.0 * ys[1] - ys[2])/(2.0*h)
+    fddys[n] = (3.0 * ys[n] - 4.0 * ys[n-1] + ys[n-2])/(2.0*h)
+
+    errs = np.append(errs,[[h,np.linalg.norm(dys - fddys,np.inf)]],0) # Remember the error - use the inf norm so the number of grid points does not matter
     n = 2*n                   # Double the number of grid points for next test
 
 print(errs)
@@ -32,7 +39,4 @@ plt.loglog(errs[:,0],errs[:,1],'-*')
 plt.loglog(errs[:,0],errs[:,0]**2,'-r')
 plt.loglog(errs[:,0],errs[:,0],'-r')
 plt.show()
-
-# Homework: add a finite difference approximation to the derivative at grid points 0 and n while keeping the error O(h^2)!
-
-
+# Note the typical V-shape: an h too large give a large error of approximation (of the generating function by a polynomial) but an h too small give cancellation error. There is an optimal value for h and you will need to find/estimate it for every problem!
